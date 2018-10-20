70bfee418484d43fcebc4ba3e6446b42c34ebfc6
arobillard <robi0653@algonquinlive.com>
2018-10-20 10:25:37 -0400

    Upload LA work

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// Nav Toggle
+
+$('.masthead').removeClass('sticky')
+
+$('.tog-nav').on('click', function (e) {
+  e.preventDefault();
+  $(this).toggleClass('open');
+  $('.header-nav').toggleClass('open');
+  $('.masthead').toggleClass('open');
+});
+
+$(function() {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 50) {
+        $('.masthead').addClass('sticky')
+      }
+      if ($(this).scrollTop() < 50) {
+        $('.masthead').removeClass('sticky')
+      }
+   });
+});
+
+$('.testimonials .next').on('click', function (e) {
+  e.preventDefault();
+  var oneIsActive = $('#quote-1').hasClass('active');
+  var twoIsActive = $('#quote-2').hasClass('active');
+  var threeIsActive = $('#quote-3').hasClass('active');
+
+  if (oneIsActive) {
+    $('#quote-1').fadeOut();
+    $('#quote-1').removeClass('active');
+    $('#quote-2').addClass('active');
+    $('#quote-2').fadeIn();
+  }
+
+  if (twoIsActive) {
+    $('#quote-2').fadeOut();
+    $('#quote-2').removeClass('active');
+    $('#quote-3').addClass('active');
+    $('#quote-3').fadeIn();
+  }
+
+  if (threeIsActive) {
+    $('#quote-3').fadeOut();
+    $('#quote-3').removeClass('active');
+    $('#quote-1').addClass('active');
+    $('#quote-1').fadeIn();
+  }
+});
+
+$('.testimonials .previous').on('click', function (e) {
+  e.preventDefault();
+  var oneIsActive = $('#quote-1').hasClass('active');
+  var twoIsActive = $('#quote-2').hasClass('active');
+  var threeIsActive = $('#quote-3').hasClass('active');
+
+  if (oneIsActive) {
+    $('#quote-1').fadeOut();
+    $('#quote-1').removeClass('active');
+    $('#quote-3').addClass('active');
+    $('#quote-3').fadeIn();
+  }
+
+  if (twoIsActive) {
+    $('#quote-2').fadeOut();
+    $('#quote-2').removeClass('active');
+    $('#quote-1').addClass('active');
+    $('#quote-1').fadeIn();
+  }
+
+  if (threeIsActive) {
+    $('#quote-3').fadeOut();
+    $('#quote-3').removeClass('active');
+    $('#quote-2').addClass('active');
+    $('#quote-2').fadeIn();
+  }
+});
+
+$('.accordian-label').on('click', function() {
+  $(this).toggleClass('open');
+});
